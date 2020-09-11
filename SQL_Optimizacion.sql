91fe97bfaf84b382e80b31cf57536e6009d4a080
Roy Rojas <royrojas@gmail.com>
2020-09-11 16:20:36 -0600

    07 Versionadas

--- SQL_Optimizacion.sql
+++ SQL_Optimizacion.sql
@@ -31,6 +31,17 @@ INSERT INTO [dbo].[Usuario]
            ,'@royrojasdev'
            ,'www.dotnetcr.com')
 
+INSERT INTO [dbo].[Usuario]
+           ([UsuarioID]
+           ,[Nombre]
+           ,[Twitter]
+           ,[Web])
+     VALUES
+           (2
+           ,'Bill Gates'
+           ,'@billgates'
+           ,'www.microsoft.com.com')
+
 GO
 
 UPDATE Usuario
@@ -39,10 +50,28 @@ WHERE UsuarioID = 1
 
 GO
 
+-- Consultas a los datos historicos
+
+-- Consulta todos los cambios por rango de fechas
+SELECT * FROM Usuario
+  FOR SYSTEM_TIME
+    BETWEEN '2020-01-01 00:00:00.0000000' AND '2021-01-01 00:00:00.0000000'
+  ORDER BY ValidFrom;
+
+GO
+
+-- Consulta un usuario por rango de fechas
 SELECT * FROM Usuario
   FOR SYSTEM_TIME
     BETWEEN '2020-01-01 00:00:00.0000000' AND '2021-01-01 00:00:00.0000000'
       WHERE UsuarioID = 1 ORDER BY ValidFrom;
+
+GO
+
+-- Consulta un usuario por ID
+SELECT * FROM Usuario
+    FOR SYSTEM_TIME ALL WHERE
+        UsuarioID = 2 ORDER BY ValidFrom;
 
 
 -- Para borrar las tablas versionadas
@@ -76,6 +105,6 @@ ADD
     , ValidTo datetime2 (2) GENERATED ALWAYS AS ROW END HIDDEN
         constraint DF_ValidTo DEFAULT '9999.12.31 23:59:59.99'
     , PERIOD FOR SYSTEM_TIME (ValidFrom, ValidTo);
-    
+
 ALTER TABLE Usuario2
     SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.Usuario2_History));
