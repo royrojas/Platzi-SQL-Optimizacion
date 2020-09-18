c750fc4a93f5f6704e5ffa50180027f65e9ad18c
Roy Rojas <royrojas@gmail.com>
2020-09-17 19:27:36 -0600

    commit

--- SQL_Optimizacion.sql
+++ SQL_Optimizacion.sql
@@ -1,3 +1,28 @@
 -- Curso Optimización SQL SERVER
 -- Roy Rojas
 -- Clase 20 - Funciones
+
+USE WideWorldImporters
+
+GO
+
+CREATE FUNCTION f_TotalVendidoXProducto
+(
+	@StockItemID int
+)
+RETURNS decimal
+AS
+BEGIN
+
+	DECLARE @total decimal
+
+	SELECT @total = SUM(Quantity * UnitPrice)
+	  FROM  Sales.OrderLines
+	 WHERE StockItemID = @StockItemID
+
+	RETURN @total
+
+END
+
+
+SELECT dbo.f_TotalVendidoXProducto(45)
