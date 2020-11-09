b73374fb0fc4423d7c489d07a0af3619c4aff686
Roy Rojas <royrojas@gmail.com>
2020-11-09 02:18:04 -0600

    COMMIT

--- SQL_Optimizacion.sql
+++ SQL_Optimizacion.sql
@@ -1,16 +1,35 @@
 -- Curso Optimización SQL SERVER
 -- Roy Rojas
--- Clase 07 - Crear Indices
+-- Clase 09 - Indices pueden perjudicar el rendimiento
 
-USE WideWorldImporters
+USE AdventureWorks2019
 
 GO
 
 SET STATISTICS IO ON
 
-SELECT TOP 10000 * 
-  FROM Application.People p INNER JOIN 
-       Sales.InvoiceLines i ON p.PersonID = i.LastEditedBy INNER JOIN 
-       Warehouse.StockItemTransactions s ON p.PersonID = s.LastEditedBy
- ORDER BY i.StockItemID
+SELECT SalesOrderDetailID,
+	   OrderQty
+  FROM Sales.SalesOrderDetail S
+ WHERE ProductID = (SELECT AVG(ProductID)
+					 FROM Sales.SalesOrderDetail S2
+					 WHERE S2.SalesOrderID = S.SalesOrderID
+					 GROUP BY SalesOrderID)
 
+-- se pueden ver los datos y analizar la cantidad e informacion
+-- una pagina pesa 8K. se multiplican los valores para ver el tamaño de la informacion que estamos procesando.
+
+CREATE NONCLUSTERED INDEX IX_PRIMERO
+ON Sales.SalesOrderDetail
+(SalesOrderID ASC, ProductID ASC)
+INCLUDE (SalesOrderDetailID, OrderQty)
+
+
+CREATE NONCLUSTERED INDEX IX_SEGUNDO
+ON Sales.SalesOrderDetail
+(ProductID ASC, SalesOrderID ASC)
+INCLUDE (SalesOrderDetailID, OrderQty)
+
+
+DROP INDEX IX_PRIMERO ON Sales.SalesOrderDetail
+DROP INDEX IX_SEGUNDO ON Sales.SalesOrderDetail
