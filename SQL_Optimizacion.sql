7037ea7115237411914295a36560449a1f8dfc0f
Roy Rojas <royrojas@gmail.com>
2020-11-09 01:39:43 -0600

    commit

--- SQL_Optimizacion.sql
+++ SQL_Optimizacion.sql
@@ -1,44 +1,14 @@
 -- Curso Optimización SQL SERVER
 -- Roy Rojas
--- Clase 06 - Indices
+-- Clase 08 - Forzar Indices
 
 USE WideWorldImporters
 
 GO
 
-SELECT TOP 10000 * 
-  FROM Application.People p INNER JOIN 
-       Sales.InvoiceLines i ON p.PersonID = i.LastEditedBy INNER JOIN 
-       Warehouse.StockItemTransactions s ON p.PersonID = s.LastEditedBy
- ORDER BY i.StockItemID
-
- GO
-
--- Ejecute el siguiente comando para habilitar las estadísticas para el IO.
 SET STATISTICS IO ON
 
-SELECT [OrderID],[ContactPersonID],
-        [PickingCompletedWhen]
-FROM [WideWorldImporters].[Sales].[Orders]
-WHERE ContactPersonID = 3176;
-
--- Corregir el índice
-FK_Sales_Orders_ContactPersonID agregar [PickingCompletedWhen]
-
--- Aqui podemos ver la respuesta de las estadísticas.
--- Table 'Orders'. Scan count 1, logical reads 416,
--- En el plan de ejecucion podemos ver el Key Lookup y las columnas que estan fuera del índice.
--- Corregir el índice
-
--------------------------
-
-SET STATISTICS IO ON
-SELECT [InvoiceID]     
-      ,[ContactPersonID]
-      ,[AccountsPersonID]
-  FROM [WideWorldImporters].[Sales].[Invoices]
-  WHERE [ContactPersonID] >= 3032 AND
-        [ContactPersonID] <= 3035;
-
--- Corregir el índice
-FK_Sales_Invoices_ContactPersonID agregar [AccountsPersonID]
+SELECT *
+FROM [WideWorldImporters].[Sales].[Invoices]
+WITH(INDEX([FK_Sales_Invoices_AccountsPersonID]))
+WHERE CustomerID = 191
