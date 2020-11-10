da286c9aad55f6ebb1fdce2c5b2fa5da0be54ad9
Roy Rojas <royrojas@gmail.com>
2020-11-09 22:58:39 -0600

    commit

--- SQL_Optimizacion.sql
+++ SQL_Optimizacion.sql
@@ -80,3 +80,52 @@ GO
 SELECT * FROM UsuarioTarget
 SELECT * FROM UsuarioSource
 
+
+
+--------------------
+-- Sintaxis
+
+-- SQL Server and Azure SQL Database
+[ WITH <common_table_expression> [,...n] ]  
+MERGE
+    [ TOP ( expression ) [ PERCENT ] ]
+    [ INTO ] <target_table> [ WITH ( <merge_hint> ) ] [ [ AS ] table_alias ]  
+    USING <table_source> [ [ AS ] table_alias ]
+    ON <merge_search_condition>  
+    [ WHEN MATCHED [ AND <clause_search_condition> ]  
+        THEN <merge_matched> ] [ ...n ]  
+    [ WHEN NOT MATCHED [ BY TARGET ] [ AND <clause_search_condition> ]  
+        THEN <merge_not_matched> ]  
+    [ WHEN NOT MATCHED BY SOURCE [ AND <clause_search_condition> ]  
+        THEN <merge_matched> ] [ ...n ]  
+    [ <output_clause> ]  
+    [ OPTION ( <query_hint> [ ,...n ] ) ]
+;  
+  
+<target_table> ::=  
+{
+    [ database_name . schema_name . | schema_name . ]  
+  target_table  
+}  
+  
+<merge_hint>::=  
+{  
+    { [ <table_hint_limited> [ ,...n ] ]  
+    [ [ , ] INDEX ( index_val [ ,...n ] ) ] }  
+}  
+
+<merge_search_condition> ::=  
+    <search_condition>  
+  
+<merge_matched>::=  
+    { UPDATE SET <set_clause> | DELETE }  
+  
+<merge_not_matched>::=  
+{  
+    INSERT [ ( column_list ) ]
+        { VALUES ( values_list )  
+        | DEFAULT VALUES }  
+}  
+  
+<clause_search_condition> ::=  
+    <search_condition>
