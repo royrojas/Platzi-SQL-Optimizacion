136f8cce2cfccf2124f744bf2e43f81a8366f9a3
Roy Rojas <royrojas@gmail.com>
2020-11-09 23:18:03 -0600

    commit

--- SQL_Optimizacion.sql
+++ SQL_Optimizacion.sql
@@ -55,3 +55,27 @@ SELECT ProductID, SUM(OrderQty) FROM Sales.SalesOrderDetail AS sod
 
 -------
 
+
+CREATE OR ALTER PROCEDURE MerceUsuarioTarget
+    @Codigo integer,
+    @Nombre varchar(100),
+    @Puntos integer
+AS
+BEGIN
+    MERGE UsuarioTarget AS T
+        USING (SELECT @Codigo, @Nombre, @Puntos) AS S 
+					   (Codigo, Nombre, Puntos)
+		ON (T.Codigo = S.Codigo)
+    WHEN MATCHED THEN
+        UPDATE SET T.Nombre = S.Nombre,
+				   T.Puntos = S.Puntos
+    WHEN NOT MATCHED THEN
+        INSERT (Codigo, Nombre, Puntos)
+        VALUES (S.Codigo, S.Nombre, S.Puntos) ;
+END
+
+GO 
+
+select * from UsuarioTarget
+exec MerceUsuarioTarget 3,'Roy Rojas', 9
+select * from UsuarioTarget
