d87164bd811125827ef1a103b35f94ae7dba45c5
Roy Rojas <royrojas@gmail.com>
2020-11-11 09:25:55 -0600

    funciones tabla

--- SQL_Optimizacion.sql
+++ SQL_Optimizacion.sql
@@ -1,49 +1,90 @@
 -- Curso Optimización SQL SERVER
 -- Roy Rojas
--- Clase 20 - Funciones
+-- Clase 21 - Funciones Tabla
 
 USE WideWorldImporters
 
 GO
 
-SELECT I.StockItemID,
-	   I.StockItemName,
-	   SUM(O.Quantity * O.UnitPrice) as Vendido
-  FROM Warehouse.StockItems I INNER JOIN	
-	   Sales.OrderLines O ON I.StockItemID = O.StockItemID
- WHERE I.StockItemID = 45
- GROUP BY I.StockItemID,
-	   I.StockItemName
-GO
-SELECT I.StockItemID,
-	   I.StockItemName,
-	   dbo.f_TotalVendidoXProducto(I.StockItemID) as Vendido
-  FROM Warehouse.StockItems I
- WHERE I.StockItemID = 45
+-- Funcion con retorno de una tabla
 
- GO
- 
--- Funcion con retorno de un valor
-CREATE FUNCTION f_TotalVendidoXProducto
-(
-	@StockItemID int
+	SELECT s.StockItemID, s.StockItemName, SUM(l.Quantity) as Cantidad
+	  FROM Warehouse.StockItems s INNER JOIN
+		   Sales.InvoiceLines l ON s.StockItemID = l.StockItemID INNER JOIN
+		   Sales.Invoices i ON l.InvoiceID = i.InvoiceID INNER JOIN
+		   Sales.Customers c ON i.CustomerID = c.CustomerID
+	 WHERE c.CustomerID = 832
+	 GROUP BY s.StockItemID, s.StockItemName
+
+GO
+
+
+CREATE or ALTER FUNCTION f_TotalComprasXCliente
+(	
+	@CustomerID int
 )
-RETURNS decimal
+RETURNS TABLE 
+AS
+RETURN 
+(
+	SELECT s.StockItemID, s.StockItemName, SUM(l.Quantity) as Cantidad
+	  FROM Warehouse.StockItems s INNER JOIN
+		   Sales.InvoiceLines l ON s.StockItemID = l.StockItemID INNER JOIN
+		   Sales.Invoices i ON l.InvoiceID = i.InvoiceID INNER JOIN
+		   Sales.Customers c ON i.CustomerID = c.CustomerID
+	 WHERE c.CustomerID = @CustomerID
+	 GROUP BY s.StockItemID, s.StockItemName
+)
+
+GO
+
+SELECT * FROM dbo.f_TotalComprasXCliente(832)
+
+GO
+
+
+-- Funciones con recursividad
+
+USE AdventureWorks2019
+
+GO
+
+CREATE OR ALTER FUNCTION dbo.ufn_FindReports (@InEmpID INTEGER)
+RETURNS @retFindReports TABLE
+(
+    EmployeeID int primary key NOT NULL,
+    FirstName nvarchar(255) NOT NULL,
+    LastName nvarchar(255) NOT NULL,
+    JobTitle nvarchar(50) NOT NULL,
+    RecursionLevel int NOT NULL
+)
+--Returns a result set that lists all the employees who report to the
+--specific employee directly or indirectly.*/
 AS
 BEGIN
+WITH EMP_cte(EmployeeID, OrganizationNode, FirstName, LastName, JobTitle, RecursionLevel) -- CTE name and columns
+    AS (
+        -- Get the initial list of Employees for Manager n
+        SELECT e.BusinessEntityID, e.OrganizationNode, p.FirstName, p.LastName, e.JobTitle, 0
+        FROM HumanResources.Employee e INNER JOIN 
+		     Person.Person p ON p.BusinessEntityID = e.BusinessEntityID
+        WHERE e.BusinessEntityID = @InEmpID
+        UNION ALL
+        -- Join recursive member to anchor
+        SELECT e.BusinessEntityID, e.OrganizationNode, p.FirstName, p.LastName, e.JobTitle, RecursionLevel + 1
+        FROM HumanResources.Employee e INNER JOIN
+			 EMP_cte ON e.OrganizationNode.GetAncestor(1) = EMP_cte.OrganizationNode INNER JOIN 
+			 Person.Person p ON p.BusinessEntityID = e.BusinessEntityID
+        )
+	
+	-- Copiamos los valores en la tabla resultado
+    INSERT @retFindReports
+    SELECT EmployeeID, FirstName, LastName, JobTitle, RecursionLevel
+    FROM EMP_cte
+    RETURN
 
-	DECLARE @total decimal
-
-	SELECT @total = SUM(Quantity * UnitPrice)
-	  FROM  Sales.OrderLines
-	 WHERE StockItemID = @StockItemID
-
-	RETURN @total
-
-END
-
+END;
 GO
-
-SELECT dbo.f_TotalVendidoXProducto(45)
-
-GO
+-- Example invocation
+SELECT EmployeeID, FirstName, LastName, JobTitle, RecursionLevel
+FROM dbo.ufn_FindReports(2);
