c9bc0d5b37d8cf408ada050b8739096de3da359f
Roy Rojas <royrojas@gmail.com>
2020-11-23 19:55:35 -0600

    Final 08

--- SQL_Optimizacion.sql
+++ SQL_Optimizacion.sql
@@ -8,12 +8,13 @@ GO
 
 SET STATISTICS IO ON
 
+-- En este ejemplo indicamos que queremos utulizar el índice FK_Sales_Invoices_AccountsPersonID
+
 SELECT *
 FROM [WideWorldImporters].[Sales].[Invoices]
 WITH(INDEX([FK_Sales_Invoices_AccountsPersonID]))
 WHERE CustomerID = 191
  
--- Reiterando que forzar el uso de un indice NO es una buena idea, 
--- exepto en casos aislados de uso temporal. 
+-- Reiterando que forzar el uso de un indice NO es una buena idea, exepto en casos aislados de uso temporal. 
 -- Imaginemos una migracion o una carga especial de datos donde queremos que los datos se comporten de una forma en específico.
 -- Siempre es recomendado reescribir una consulta para que se utilice el indice adecuado.
