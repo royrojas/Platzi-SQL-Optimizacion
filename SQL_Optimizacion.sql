081dd678ca93f4a23ed1a27fe3fcb6eafbc8a0b5
Roy Rojas <royrojas@gmail.com>
2020-12-01 16:29:37 -0600

    Final 17

--- SQL_Optimizacion.sql
+++ SQL_Optimizacion.sql
@@ -8,7 +8,7 @@
 -- Creamos el catalogo con la herramienta grafica.
 
 -- En en el Management Studio buscamos en la base de datos 
-Storage / Full Text Catalogs / Click derecho New
+-- Storage / Full Text Catalogs / Click derecho New
 
 ------------------------
 -- Cómo lo utilizamos
